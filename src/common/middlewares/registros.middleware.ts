f5cb587f0a4127973e9116a1ab92ef47441ae682
Daniel Velazquez <danip293@gmail.com>
2023-09-28 23:24:33 -0600

    logger middleware updated

--- src/common/middlewares/registros.middleware.ts
+++ src/common/middlewares/registros.middleware.ts
@@ -6,11 +6,15 @@ class LogsMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction) {
-    response.on('finish', () => {
-      const { method, originalUrl } = request;
-      const { statusCode, statusMessage } = response;
+    const { method, originalUrl } = request;
+    const start = Date.now();
 
-      const message = `${method} ${originalUrl} ${statusCode} ${statusMessage}`;
+    response.on('finish', () => {
+      const { statusCode, statusMessage } = response;
+      const end = Date.now();
+      const duration = end - start;
+
+      const message = `${method} ${originalUrl} ${statusCode} ${statusMessage} - ${duration}ms`;
 
       if (statusCode >= 500) {
         return this.logger.error(message);
